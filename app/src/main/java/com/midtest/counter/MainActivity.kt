ba150a6265e07074f04646941ada7914de233dc7
mauraqp <mauraqoonitah@gmail.com>
2020-10-28 13:11:20 +0700

    innitial commit

---
+++ app/src/main/java/com/midtest/counter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.midtest.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
